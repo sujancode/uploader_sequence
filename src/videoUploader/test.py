b3afacbdfcb93a6414effe72a768f43d42aacbde
Sujan <sujancode@gmail.com>
2022-12-21 08:04:00 -0500

    Made changes to vpn

--- src/videoUploader/test.py
+++ src/videoUploader/test.py
@@ -2,6 +2,8 @@ from sign_up_upload import sign_up
 from dependency.database.index import getDatabaseWrapperInstance
 import requests
 import random
+import subprocess
+
 while True:
     db=getDatabaseWrapperInstance("created_video")
     video_list=db.find_all(collection="videos")
@@ -13,6 +15,8 @@ while True:
         with open("/tmp/tmp.mp4","wb") as video_file:
             video_file.write(res.content)
         sign_up(video_url=data["url"],title=data['title'],tags=data['tags'])
+        result=subprocess.run(["nordvpn","c","united_states"])
+        print(result)
     except Exception as e:
         print(e)
 
