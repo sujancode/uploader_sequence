13306793592a4d1f363217040a287330cd10ec61
Sujan <sujancode@gmail.com>
2022-12-21 08:36:34 -0500

    Eporner chnages

--- src/epornerUploder/temp_mail.py
+++ src/epornerUploder/temp_mail.py
@@ -12,13 +12,10 @@ def get_email():
 def get_activation_link(email):
     activation_link=""
     while not activation_link:
-        try:
             message=requests.get(f'https://api.internal.temp-mail.io/api/v3/email/{email}/messages')
             message=json.loads(message.text)
             msg_html=message[0]['body_html']
             soup=bs4.BeautifulSoup(msg_html,'html.parser')
             activation_link=soup.select_one('[href^="https://www.eporner.com/login/verify/"]')['href']
             time.sleep(2)
-        except Exception as e:
-            print(e)
     return activation_link
