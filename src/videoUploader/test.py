eabdcfaf3eeffb42b2b535e407d81a066e19712d
Sujan <sujancode@gmail.com>
2023-01-13 02:31:07 -0500

    made changes

--- src/videoUploader/test.py
+++ src/videoUploader/test.py
@@ -33,35 +33,38 @@ db=getDatabaseWrapperInstance("created_video")
 #     except Exception as e:
 #         print(e)
 
-while True:
-    video_list=db.find_all(collection="videos")
-    rand_index=random.randint(0,len(video_list)-1)
-    print(rand_index)
-    data=video_list[rand_index]
+try:
+    while True:
+        video_list=db.find_all(collection="videos")
+        rand_index=random.randint(0,len(video_list)-1)
+        print(rand_index)
+        data=video_list[rand_index]
 
-    result=subprocess.run(["nordvpn","d"])
+        result=subprocess.run(["nordvpn","d"])
 
-    try:
-        filename=data["url"].split("/")[-1]
-        filename=filename.replace("+","")
+        try:
+            filename=data["url"].split("/")[-1]
+            filename=filename.replace("+","")
 
-        res=requests.get(data["url"])
-        with open(f"{BASE_DIR}/tmp/tmp.mp4","wb") as video_file:
-            video_file.write(res.content)
-        
-        bucket_name="new-data-source"
-        url=f"https://{bucket_name}.s3.amazonaws.com/{filename}"
-        print(url)
-        storage_bucket=getS3StorageInstance()
-        storage_bucket.upload_file(path=f'{BASE_DIR}/tmp/tmp.mp4',bucket_name=bucket_name,upload_location=filename)
+            res=requests.get(data["url"])
+            with open(f"{BASE_DIR}/tmp/tmp.mp4","wb") as video_file:
+                video_file.write(res.content)
+            
+            bucket_name="new-data-source"
+            url=f"https://{bucket_name}.s3.amazonaws.com/{filename}"
+            print(url)
+            storage_bucket=getS3StorageInstance()
+            storage_bucket.upload_file(path=f'{BASE_DIR}/tmp/tmp.mp4',bucket_name=bucket_name,upload_location=filename)
 
-        countries=get_list_countries()
-        coutry=random.choice(countries)
-        result=subprocess.run(["nordvpn","c",coutry])
+            countries=get_list_countries()
+            coutry=random.choice(countries)
+            result=subprocess.run(["nordvpn","c",coutry])
 
-        print(result) 
-        sign_up(video_url=url,title=data['title'],tags=data['tags'],username=data.get('username',""))
-        
-    except Exception as e:
-        print(e)
-
+            print(result) 
+            sign_up(video_url=url,title=data['title'],tags=data['tags'],username=data.get('username',""))
+            
+        except Exception as e:
+            print(e)
+except Exception as e:
+    print(e)
+    print("Most Probably database error")
